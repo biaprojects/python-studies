cc1fa78dfd6453a49b1bf1db4e9fd259c0ad0723
Bia Almeida <beatriz.06.06.2016@gmail.com>
2025-09-16 13:49:08 -0300

    :sparkles: Add Hello_world.py

---
+++ a - First-steps/Hello_world.py
@@ -0,0 +1,10 @@
+print("Hello world!")
+
+name = input("What's your name? ")
+print("Hello, " + name)
+
+age = int(input("How old are you? "))
+print("You're", age, "years old")
+
+height = float(input("What's your height? (m) "))
+print(f"Ok, {name}. You are {age} years old and {height}m tall.")
