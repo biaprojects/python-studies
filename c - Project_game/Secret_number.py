ec5baa8708ff1dab6891bef113ae763ecf700624
Bia Almeida <beatriz.06.06.2016@gmail.com>
2025-09-17 12:39:02 -0300

    :sparkles: Add game

---
+++ c - Project_game/Secret_number.py
@@ -0,0 +1,26 @@
+import random
+
+number = random.randint(1, 100)  # de O a 100 está incluso no sorteio
+
+print("What's your name?")
+name = input()
+print("Hey", name, "Let's start our game to guess the secret number!")
+
+print("What's your first guess? Choose a number from 1 to 100")
+guess = int(input())
+sum = 1
+if guess == number:
+    print("Uhuuu! Congrats", name + "!!!")
+    print("You guessed the secret number at your first try! (" + str(number) + ")")
+else:
+    while guess != number:
+        print("Ops, you didn't get the secret number...")
+        if guess > number:
+            print("The secret number is LOWER")
+        else:
+            print("The secret number is HIGHER")
+        print("What's your next guess?")
+        guess = int(input())
+        sum += 1
+    print("Uhuuu! Congrats", name + "!!!")
+    print("You guessed the secret number! (" + str(number) + "). You took", sum, "tries.")
