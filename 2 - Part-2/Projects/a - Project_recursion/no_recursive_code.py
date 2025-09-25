58c29050172931aed31f0229399680d8fcaf4f31
Bia Almeida <beatriz.06.06.2016@gmail.com>
2025-09-25 17:24:06 -0300

    :sparkles: Add recursion project

--- 2 - Part-2/Projects/a - Project_recursion/no_recursive_code.py
+++ 2 - Part-2/Projects/a - Project_recursion/no_recursive_code.py
@@ -4,29 +4,29 @@ number = int(input("Type the number you want to calculate the factorial (n!) "))
 factorial = 1
 
 # values that will be printed
-values = [factorial]
+to_print = [factorial]
 
 # values of the factorials
-factorials = []
+results = []
 
 # Exceptions:
-if number == 0:
+if number == 0 or number == 1:
     print(f'Calculating factorial of {number}')
     print(f'Factorial of {number} is 1 (base case)')
 
 # calculating
 else:
-    for numbers in range(2, number+1):
-        factorial *= number
-        factorials.append(factorial)
-        values.append(numbers)
+    for value in range(2, number+1):
+        factorial *= value
+        results.append(factorial)
+        to_print.append(value)
 
-    values = values[::-1]
+    values = results[::-1]
 
-    for value in values:
+    for value in to_print:
         print(f'Calculating factorial of {value}')
     for i in range(1, number + 1):
         if i == 1:
             print(f'Factorial of {i} is 1 (base case)')
         else:
-            print(f'Factorial of {i} is {factorials[-2+i]}')
+            print(f'Factorial of {i} is {results[-2+i]}')
